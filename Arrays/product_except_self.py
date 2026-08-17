0d35d9c0f6e483f835191d0d74502f5fc1cc0afa
Devasena K <156397888+Devasena-K09@users.noreply.github.com>
2026-08-17 16:34:02 +0530

    Added Product Except Self solution

--- Arrays/product_except_self.py
+++ Arrays/product_except_self.py
@@ -1,1 +1,16 @@
+def product_except_self(nums):
+    result = [1] * len(nums)
 
+    prefix = 1
+    for i in range(len(nums)):
+        result[i] = prefix
+        prefix *= nums[i]
+
+    suffix = 1
+    for i in range(len(nums)-1, -1, -1):
+        result[i] *= suffix
+        suffix *= nums[i]
+
+    return result
+
+print(product_except_self([1,2,3,4]))
