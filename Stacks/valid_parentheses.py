606a510a7ec909f81a380d3e196b6a05221d0cfa
Devasena K <156397888+Devasena-K09@users.noreply.github.com>
2026-08-19 13:15:35 +0530

    Add Valid Parentheses stack problem

--- Stacks/valid_parentheses.py
+++ Stacks/valid_parentheses.py
@@ -1,1 +1,24 @@
+def is_valid(s):
 
+    stack = []
+
+    mapping = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    }
+
+    for char in s:
+
+        if char in mapping.values():
+            stack.append(char)
+
+        elif char in mapping:
+
+            if not stack or stack.pop() != mapping[char]:
+                return False
+
+    return len(stack) == 0
+
+
+print(is_valid("()[]{}"))
