dbdb612fdc22512e58154608dd832e5ee97fe16d
Devasena K <156397888+Devasena-K09@users.noreply.github.com>
2026-08-19 13:01:55 +0530

    Add First Occurrence Binary Search problem

---
+++ Searching/first_occurrence.py
@@ -0,0 +1,16 @@
+def first_occurrence(nums, target):
+    left, right = 0, len(nums)-1
+    ans = -1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if nums[mid] == target:
+            ans = mid
+            right = mid - 1
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return ans
