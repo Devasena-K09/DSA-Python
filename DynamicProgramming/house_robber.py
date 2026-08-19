af63d053ab4f8b27a93474af346a9a00abaab641
Devasena K <156397888+Devasena-K09@users.noreply.github.com>
2026-08-19 14:28:35 +0530

    Refactor rob function for dynamic programming approach
    Refactor house robber solution to use dynamic programming.

--- DynamicProgramming/house_robber.py
+++ DynamicProgramming/house_robber.py
@@ -1,15 +1,19 @@
-class Solution:
-    def rob(self, nums):
-        prev1 = 0
-        prev2 = 0
+def rob(nums):
+    if not nums:
+        return 0
 
-        for num in nums:
-            temp = max(prev1, prev2 + num)
-            prev2 = prev1
-            prev1 = temp
+    if len(nums) == 1:
+        return nums[0]
 
-        return prev1
+    dp = [0] * len(nums)
+    dp[0] = nums[0]
+    dp[1] = max(nums[0], nums[1])
+
+    for i in range(2, len(nums)):
+        dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+
+    return dp[-1]
 
 
-# Example
-print(Solution().rob([2, 7, 9, 3, 1]))
+nums = [2, 7, 9, 3, 1]
+print(rob(nums))
