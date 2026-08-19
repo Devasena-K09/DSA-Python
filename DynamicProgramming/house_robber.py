7a14063a7f197841206e5108faacfdd43e9f48e4
Devasena K <156397888+Devasena-K09@users.noreply.github.com>
2026-08-19 14:24:10 +0530

    Add house robber solution in house_robber.py
    Implement dynamic programming solution for house robber problem.

--- DynamicProgramming/house_robber.py
+++ DynamicProgramming/house_robber.py
@@ -1,1 +1,15 @@
+class Solution:
+    def rob(self, nums):
+        prev1 = 0
+        prev2 = 0
 
+        for num in nums:
+            temp = max(prev1, prev2 + num)
+            prev2 = prev1
+            prev1 = temp
+
+        return prev1
+
+
+# Example
+print(Solution().rob([2, 7, 9, 3, 1]))
