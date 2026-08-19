3b63f8ac5d7c880588ef017e41409aaf2add6e26
Devasena K <156397888+Devasena-K09@users.noreply.github.com>
2026-08-19 14:06:43 +0530

    Add BFS traversal

--- Graphs/bfs.py
+++ Graphs/bfs.py
@@ -1,1 +1,30 @@
+from collections import deque
 
+graph = {
+    0: [1, 2],
+    1: [3],
+    2: [],
+    3: []
+}
+
+def bfs(start):
+
+    visited = set()
+    queue = deque([start])
+
+    visited.add(start)
+
+    while queue:
+
+        node = queue.popleft()
+
+        print(node, end=" ")
+
+        for neighbor in graph[node]:
+
+            if neighbor not in visited:
+
+                visited.add(neighbor)
+                queue.append(neighbor)
+
+bfs(0)
