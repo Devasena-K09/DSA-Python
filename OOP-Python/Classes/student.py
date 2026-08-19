c8a023fb9a701b9e366d358515d27bc660d4669d
Devasena K <156397888+Devasena-K09@users.noreply.github.com>
2026-08-19 15:18:14 +0530

    Add Student class with display method

---
+++ OOP-Python/Classes/student.py
@@ -0,0 +1,13 @@
+class Student:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display(self):
+        print(self.name, self.age)
+
+
+student = Student("Devasena", 22)
+
+student.display()
