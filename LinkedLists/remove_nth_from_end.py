600c8fe4df5e84079731d40275a876737eaf2979
Devasena K <156397888+Devasena-K09@users.noreply.github.com>
2026-08-19 13:44:49 +0530

    Add Remove Nth Node From End problem

---
+++ LinkedLists/remove_nth_from_end.py
@@ -0,0 +1,17 @@
+def remove_nth_from_end(head, n):
+
+    dummy = ListNode(0)
+    dummy.next = head
+
+    first = second = dummy
+
+    for _ in range(n + 1):
+        first = first.next
+
+    while first:
+        first = first.next
+        second = second.next
+
+    second.next = second.next.next
+
+    return dummy.next
