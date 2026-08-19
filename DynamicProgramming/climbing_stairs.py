fa0e64f9502c408010feba713359bac7352f1b05
Devasena K <156397888+Devasena-K09@users.noreply.github.com>
2026-08-19 14:23:04 +0530

    Update climbing_stairs.py

--- DynamicProgramming/climbing_stairs.py
+++ DynamicProgramming/climbing_stairs.py
@@ -1,1 +1,18 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
 
+        first = 1
+        second = 2
+
+        for _ in range(3, n + 1):
+            third = first + second
+            first = second
+            second = third
+
+        return second
+
+
+# Example
+print(Solution().climbStairs(5))
