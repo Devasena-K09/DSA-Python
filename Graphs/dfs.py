65235526c4d9beb91c4bd7b0f62d7ec6eb6f0e2b
Devasena K <156397888+Devasena-K09@users.noreply.github.com>
2026-08-19 14:06:22 +0530

    Add DFS traversal

--- Graphs/dfs.py
+++ Graphs/dfs.py
@@ -1,1 +1,22 @@
+graph = {
+    0: [1, 2],
+    1: [3],
+    2: [],
+    3: []
+}
 
+visited = set()
+
+def dfs(node):
+
+    if node in visited:
+        return
+
+    visited.add(node)
+
+    print(node, end=" ")
+
+    for neighbor in graph[node]:
+        dfs(neighbor)
+
+dfs(0)
