4386c4aa42386525d81520d6456fac24e3c7bff6
Devasena K <156397888+Devasena-K09@users.noreply.github.com>
2026-08-19 14:01:54 +0530

    Add Level Order Traversal

---
+++ Trees/level_order_traversal.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+def level_order(root):
+
+    if not root:
+        return []
+
+    result = []
+    queue = deque([root])
+
+    while queue:
+
+        level = []
+
+        for _ in range(len(queue)):
+
+            node = queue.popleft()
+
+            level.append(node.value)
+
+            if node.left:
+                queue.append(node.left)
+
+            if node.right:
+                queue.append(node.right)
+
+        result.append(level)
+
+    return result
