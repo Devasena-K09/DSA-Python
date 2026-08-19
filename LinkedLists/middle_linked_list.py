da351302c1152d09b1986cb4f356a0763a6cc44f
Devasena K <156397888+Devasena-K09@users.noreply.github.com>
2026-08-19 13:41:39 +0530

    Add Middle of Linked List problem

--- LinkedLists/middle_linked_list.py
+++ LinkedLists/middle_linked_list.py
@@ -1,1 +1,17 @@
+class Node:
 
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+def find_middle(head):
+
+    slow = head
+    fast = head
+
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+
+    return slow.data
