417a7a60291bed68a420cbda526b05c0876a32d9
Devasena K <156397888+Devasena-K09@users.noreply.github.com>
2026-08-19 13:04:56 +0530

    Add Selection Sort implementation

--- Sorting/selection_sort.py
+++ Sorting/selection_sort.py
@@ -1,1 +1,20 @@
+def selection_sort(arr):
+    n = len(arr)
 
+    for i in range(n):
+
+        min_index = i
+
+        for j in range(i + 1, n):
+
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+    return arr
+
+
+nums = [29, 10, 14, 37, 13]
+
+print(selection_sort(nums))
