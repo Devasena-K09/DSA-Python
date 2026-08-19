80cfb299f713590bf41bbb488afed6339aa2e3a8
Devasena K <156397888+Devasena-K09@users.noreply.github.com>
2026-08-19 14:32:52 +0530

    Add invert_tree function for binary tree inversion
    Implement a function to invert a binary tree and test it.

---
+++ Trees/invert_binary_tree.py
@@ -0,0 +1,25 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def invert_tree(root):
+    if root:
+        root.left, root.right = root.right, root.left
+
+        invert_tree(root.left)
+        invert_tree(root.right)
+
+    return root
+
+
+root = TreeNode(4)
+root.left = TreeNode(2)
+root.right = TreeNode(7)
+
+invert_tree(root)
+
+print(root.left.val)
+print(root.right.val)
